be1db6d82573711d3e696e38713b3607d876d478
Claudir Pereira <claudirp76@gmail.com>
2026-03-29 18:21:11 -0300

    correção de layout das menssagens e criação da animação de salação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Button from "./components/button"
 import Input from "./components/input";
 import CardErro from "./components/card_error";
 import Carregar from "./components/animacao";
-import { useEffect, useRef, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 export default function Home() {
 
   const [texto, settexto] = useState('')
@@ -16,6 +16,8 @@ export default function Home() {
   const [animacao, setanimacao] = useState(false)
   const [img, setimg] = useState(false)
   const [carregando, setcarregando] = useState(true)
+  const [saldacao, setsaldacao] = useState(true)
+  const [render, setrender] = useState(true)
   const [user, setuser] = useState<Mensagem[]>([])
   const chatref = useRef<HTMLDivElement>(null)
 
@@ -31,12 +33,21 @@ export default function Home() {
     })
   },[user])
 
+  useEffect(() => {
+    if(!saldacao) {
+      const timer = setTimeout(() => {
+        setrender(false)
+      }, 700)
+      return () => (clearTimeout(timer))
+    } 
+  },[saldacao])
+
   if(carregando) {
     return <Carregar/>
   }
 
     type Mensagem = {
-      texto:string, tipo: 'user' | 'bot' | 'loading',
+      texto:string, tipo: 'user' | 'bot' | 'loading' | 'error',
     }
   
   const server = async (e? : React.BaseSyntheticEvent) => {
@@ -50,6 +61,10 @@ export default function Home() {
       setTimeout(() => {setinvalido(false)}, 6000)
       return
     }
+
+      if(user.length === 0) {
+        setsaldacao(false)
+      }
 
     setuser((prev) => [
       ...prev, {
@@ -110,29 +125,26 @@ export default function Home() {
           headers: {
             'Content-Type' : 'application/json'
           },
-          body: JSON.stringify({texto})
+          body: JSON.stringify({})
         })
 
         const response = await req.json()
         console.log(response)
 
         if(!req.ok) {
-          console.log(response.error)
           setuser((prev) => [
-            ...prev, {
-              texto: '', tipo: 'bot'
-            }
+            ...prev.filter(msg => msg.tipo !== 'loading')
           ])
+          console.log(response.error)
           setmessagens('Erro interno no servidor! Estamos verificando...')
           setinvalido(true)
           setTimeout(() => {setinvalido(false)},6000)
           return
         }
 
-          setuser((prev) => [
+        setuser((prev) => [
           ...prev.filter(msg => msg.tipo !== 'loading'), {texto: response.text, tipo: 'bot'}
         ])
-
 
       } catch (error) {
         console.log('Erro ao chama a API do gemini', error)
@@ -159,43 +171,47 @@ export default function Home() {
     {invalido && (
       <CardErro>{messagens}</CardErro>
     )}
-    <div className=" flex justify-center items-center lg:mt-90 mt-72 md:mt-60 ">
-        <p className="text-white text-center sm:text-5xl text-4xl font-semibold mx-2">Seja bem vindo!! <span className=" block mt-2 text-blue-400 sm: text-4xl">pequeno gafanhoto</span></p>
-    </div>
-    <div ref={chatref}  className="flex flex-col space-y-8 m-2 p-4 pb-24 overflow-y-auto scroll-hidden">
-      {user.map((msg, index) => (
-        <div key={index} className={`${msg.tipo == 'user'? 'flex justify-end': 'flex justify-start'}`}>
+    <div ref={chatref} className="flex flex-col overflow-y-auto scroll-hidden">
+      {render && (
+        <div className={`transition-opacity duration-700 flex justify-center items-center lg:mt-90 mt-72 md:mt-60 ${saldacao ? 'opacity-100': 'opacity-0'}`}>
+          <p className="text-white text-center sm:text-5xl text-4xl font-semibold mx-2">Seja bem vindo!! <span className=" block mt-2 text-blue-400 sm: text-4xl">pequeno gafanhoto</span></p>
+        </div>
+      )}
+      <div className="flex flex-col space-y-8 m-2 mt-90 p-4 pb-24 ">
+        {user.map((msg, index) => (
+          <div key={index} className={`${msg.tipo == 'user'? 'flex justify-end': 'flex justify-start'}`}>
 
 
-          {msg.tipo === 'loading' && (
+            {msg.tipo === 'loading' && (
+                    
+                <div className="flex items-center justify-center space-x-2 ">
+                  <span className="h-3 w-3 animate-bounce duration-700 rounded-full bg-blue-50 [animation-delay:-0.3s]"></span>
+                  <span className="h-3 w-3 animate-bounce duration-700 rounded-full bg-blue-50 [animation-delay:-0.15s]"></span>
+                  <span className="h-3 w-3 animate-bounce duration-700 rounded-full bg-blue-50"></span>
+                </div>
                   
-              <div className="flex items-center justify-center space-x-2 ">
-                <span className="h-3 w-3 animate-bounce duration-700 rounded-full bg-blue-50 [animation-delay:-0.3s]"></span>
-                <span className="h-3 w-3 animate-bounce duration-700 rounded-full bg-blue-50 [animation-delay:-0.15s]"></span>
-                <span className="h-3 w-3 animate-bounce duration-700 rounded-full bg-blue-50"></span>
-              </div>
-                
-          )}
+            )}
 
-          {msg.tipo === 'user' && (
-            <div className="bg-blue-500 ml-auto max-w-[75%] p-4 rounded-2xl text-xl break-all whitespace-pre-wrap text-amber-50">
-              {msg.texto}
-            </div>
-          )}
-
-          {msg.tipo === 'bot' && (
-            <div className="flex -ml-3">
-              <div>
-                <Image src='/icone.png' alt="icone bot" width={70} height={30}></Image>
-              </div>
-              <div className="bg-slate-800 p-4 rounded-2xl max-w-[75%] text-xl break-all whitespace-pre-wrap text-amber-50">
+            {msg.tipo === 'user' && (
+              <div className="bg-blue-500 ml-auto max-w-[75%] p-4 rounded-2xl text-xl break-all whitespace-pre-wrap text-amber-50">
                 {msg.texto}
               </div>
-            </div>
+            )}
 
-          )}
-        </div>   
-      ))}
+            {msg.tipo === 'bot' && (
+              <div className="flex -ml-3">
+                <div>
+                  <Image src='/icone.png' alt="icone bot" width={70} height={30}></Image>
+                </div>
+                <div className="bg-slate-800 p-4 rounded-2xl max-w-[75%] text-xl break-all whitespace-pre-wrap text-amber-50">
+                  {msg.texto}
+                </div>
+              </div>
+
+            )}
+          </div>   
+        ))}
+      </div>  
       
     </div> 
       <div className="max-w-4xl lg:gap-14 lg:mb-14 mx-auto fixed mb-1 flex self-center gap-7 items-center p-4 bottom-0 ">
